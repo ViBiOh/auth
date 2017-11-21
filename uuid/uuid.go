34c8ab4921f1f9a6abdc956932315aff0fd0e729
Vincent Boutour <bob@vibioh.fr>
2017-11-21 22:09:02 +0100

    Fixing uuid

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -7,15 +7,15 @@ import (
 
 // New generates random UUID according to RFC 4122
 func New() (uuid string, err error) {
-	raw := [16]byte{}
-	_, err = rand.Read(raw[:16])
+	raw := make([]byte, 16)
+	_, err = rand.Read(raw)
 	if err != nil {
 		return ``, err
 	}
 
 	raw[8] = raw[8]&^0xc0 | 0x80
 	raw[6] = raw[6]&^0xf0 | 0x40
-	uuid = fmt.Sprintf(`%x-%x-%x-%x-%x`, uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
+	uuid = fmt.Sprintf(`%x-%x-%x-%x-%x`, raw[0:4], raw[4:6], raw[6:8], raw[8:10], raw[10:])
 
 	return
 }
