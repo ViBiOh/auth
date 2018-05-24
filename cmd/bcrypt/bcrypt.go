ebc6881ebab8f9e0bc54283585329850398c6240
Vincent Boutour <vboutour@meilleursagents.com>
2018-05-24 17:03:43 +0200

    Adding param check

--- cmd/bcrypt/bcrypt.go
+++ cmd/bcrypt/bcrypt.go
@@ -8,6 +8,11 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Println(`Usage is bcrypt [password]`)
+		os.Exit(1)
+	}
+
 	password := os.Args[1]
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 	if err != nil {
