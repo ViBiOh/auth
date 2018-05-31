e15d09dda5a0e3968b09cfdbccedbab9d0958720
Vincent Boutour <vboutour@meilleursagents.com>
2018-05-31 08:59:00 +0200

    Adding prompt for bcrypt

--- cmd/bcrypt/bcrypt.go
+++ cmd/bcrypt/bcrypt.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
+	"github.com/manifoldco/promptui"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func main() {
+func getPromptPassword() string {
+	prompt := promptui.Prompt{
+		Label: "Password",
+		Mask:  '*',
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Error while reading prompt: %v", err)
+		os.Exit(1)
+	}
+
+	return result
+}
+
+func getArgsPassword() string {
 	if len(os.Args) != 2 {
 		fmt.Println(`Usage is bcrypt [password]`)
 		os.Exit(1)
 	}
 
-	password := os.Args[1]
+	return os.Args[1]
+}
+
+func main() {
+	prompt := flag.Bool(`prompt`, false, `Prompt for password rather than reading CLI args`)
+	flag.Parse()
+
+	var password string
+
+	if *prompt {
+		password = getPromptPassword()
+	} else {
+		password = getArgsPassword()
+	}
+
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 	if err != nil {
-		fmt.Printf(`Error: %v`, err)
+		fmt.Printf(`Error while generating password: %v`, err)
 	}
 	fmt.Print(string(hash))
 }
