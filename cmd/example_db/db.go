dd06356c70f82ca14406b705093ab6af3e5b6c47
Vincent Boutour <bob@vibioh.fr>
2021-05-29 12:51:58 +0200

    fix: Fixing nil pointer when no provider detected
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/example_db/db.go
+++ cmd/example_db/db.go
@@ -1,22 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"flag"
-	"fmt"
-	"net/http"
 	"os"
-	"strings"
 
 	"github.com/ViBiOh/auth/v2/pkg/ident/basic"
 	"github.com/ViBiOh/auth/v2/pkg/middleware"
 	dbStore "github.com/ViBiOh/auth/v2/pkg/store/db"
 	"github.com/ViBiOh/httputils/v4/pkg/db"
 	"github.com/ViBiOh/httputils/v4/pkg/health"
-	"github.com/ViBiOh/httputils/v4/pkg/httperror"
 	"github.com/ViBiOh/httputils/v4/pkg/httputils"
 	"github.com/ViBiOh/httputils/v4/pkg/logger"
-	"github.com/ViBiOh/httputils/v4/pkg/request"
 	"github.com/ViBiOh/httputils/v4/pkg/server"
 )
 
@@ -41,76 +35,8 @@ func main() {
 	identProvider := basic.New(authProvider, "Example with a DB")
 	middlewareApp := middleware.New(authProvider, identProvider)
 
-	go appServer.Start("http", healthApp.End(), httputils.Handler(Handler(), healthApp, middlewareApp.Middleware))
+	go appServer.Start("http", healthApp.End(), httputils.Handler(nil, healthApp, middlewareApp.Middleware))
 
 	healthApp.WaitForTermination(appServer.Done())
 	server.GracefulWait(appServer.Done())
-}
-
-// Handler for dump request. Should be use with net/http
-func Handler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		value := dumpRequest(r)
-
-		logger.Info("Dump of request\n%s", value)
-
-		if _, err := w.Write([]byte(value)); err != nil {
-			httperror.InternalServerError(w, err)
-		}
-	})
-}
-
-func dumpRequest(r *http.Request) string {
-	var headers bytes.Buffer
-	for key, value := range r.Header {
-		headers.WriteString(fmt.Sprintf("%s: %s\n", key, strings.Join(value, ",")))
-	}
-
-	var params bytes.Buffer
-	for key, value := range r.URL.Query() {
-		headers.WriteString(fmt.Sprintf("%s: %s\n", key, strings.Join(value, ",")))
-	}
-
-	var form bytes.Buffer
-	if err := r.ParseForm(); err != nil {
-		form.WriteString(err.Error())
-	} else {
-		for key, value := range r.PostForm {
-			form.WriteString(fmt.Sprintf("%s: %s\n", key, strings.Join(value, ",")))
-		}
-	}
-
-	body, err := request.ReadBodyRequest(r)
-	if err != nil {
-		logger.Error("%s", err)
-	}
-
-	var outputPattern bytes.Buffer
-	outputPattern.WriteString("%s %s\n")
-	outputData := []interface{}{
-		r.Method,
-		r.URL.Path,
-	}
-
-	if headers.Len() != 0 {
-		outputPattern.WriteString("Headers\n%s\n")
-		outputData = append(outputData, headers.String())
-	}
-
-	if params.Len() != 0 {
-		outputPattern.WriteString("Params\n%s\n")
-		outputData = append(outputData, params.String())
-	}
-
-	if form.Len() != 0 {
-		outputPattern.WriteString("Form\n%s\n")
-		outputData = append(outputData, form.String())
-	}
-
-	if len(body) != 0 {
-		outputPattern.WriteString("Body\n%s\n")
-		outputData = append(outputData, body)
-	}
-
-	return fmt.Sprintf(outputPattern.String(), outputData...)
 }
