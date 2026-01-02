0846e77d58b5ddae70305fc9c0f1a28ca0db710e
Vincent Boutour <bob@vibioh.fr>
2026-01-02 14:31:04 +0100

    feat: Add way to list link
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/store/db/link.go
+++ pkg/store/db/link.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/ViBiOh/auth/v3/pkg/model"
 	"github.com/ViBiOh/httputils/v4/pkg/id"
@@ -51,6 +52,31 @@ func (s Service) GetLinkByToken(ctx context.Context, token string) (model.Link, 
 
 		return err
 	}, getLinkByTokenQuery, token)
+}
+
+const listLinkByExternalIDs = `
+SELECT
+  external_id,
+  description
+FROM
+  auth.user_link
+WHERE
+  external_id = ANY($1)
+`
+
+func (s Service) GetLinksByExternalIDs(ctx context.Context, externalIDs ...string) ([]model.Link, error) {
+	var items []model.Link
+
+	return items, s.db.List(ctx, func(rows pgx.Rows) error {
+		var item model.Link
+
+		if err := rows.Scan(&item.ExternalID, &item.Description); err != nil {
+			return fmt.Errorf("scan: %w", err)
+		}
+		items = append(items, item)
+
+		return nil
+	}, listLinkByExternalIDs, externalIDs)
 }
 
 const deleteLinkQuery = `
